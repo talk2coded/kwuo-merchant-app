6a45d70205c6b89cb46d436f291ea9c40f0acc6e
talk2coded <emmanuelsteve43@gmail.com>
2020-07-29 22:01:43 +0100

    reg form

--- src/components/register-form/register-form.component.jsx
+++ src/components/register-form/register-form.component.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './register-form.css';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
 
 class RegisterForm extends React.Component{
         constructor(props){
