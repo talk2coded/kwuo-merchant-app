6772c24cfb5833cbe8222581a9d631bcdf818df6
talk2coded <emmanuelsteve43@gmail.com>
2020-08-12 11:51:28 +0100

    fixed the registration form @media query

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './home-page.css';
-import DashboardNav from '../../components/dashboard-header/dashboard-header';
 import HomeComponent from '../../components/home-component/home.component';
 
 class HomePage extends React.Component{
    render(){
        return(
         <div>
-            <DashboardNav/>
             <HomeComponent/>
         </div>
        )
