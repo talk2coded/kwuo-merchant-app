e9f9bb13fe325875531b5ca75cef4bc40e2dc86c
talk2coded <emmanuelsteve43@gmail.com>
2020-08-21 12:47:37 +0100

    fixed homepage

--- src/components/home-component/home.component.jsx
+++ src/components/home-component/home.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import './home.component.css';
+import kwuobal from '../../assets/images/kwuo bal2.png';
+import sendkwuo from '../../assets/images/send units.svg';
 import clsx from 'clsx';
+import Hidden from '@material-ui/core/Hidden';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
@@ -135,6 +139,10 @@ export default function Dashboard() {
   return (
     <div className={classes.root}>
       <CssBaseline />
+
+      {/* desktop version of the navbar*/}
+
+      <Hidden xsDown>
       <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)} >
         <Toolbar className={classes.toolbar}>
           <IconButton
@@ -157,10 +165,12 @@ export default function Dashboard() {
           </IconButton>
         </Toolbar>
       </AppBar>
+     
       <Drawer 
         variant="permanent"
+       
         classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose), 
         }}
         open={open} 
        
@@ -179,32 +189,165 @@ export default function Dashboard() {
         
         <List>{secondaryListItems}</List>
       </Drawer>
+      </Hidden>
+
+       {/* End of desktop version of the navbar*/}
+
+        {/* mobile version of the navbar*/}
+
+        <Hidden mdUp>
+        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)} >
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            >
+              <MenuIcon style={{color: 'black'}} />
+            </IconButton>
+            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+              Dashboard
+            </Typography>
+            <IconButton color="inherit">
+                <Settings style={{marginRight:'30px', color:'black'}} />
+            </IconButton>
+            <IconButton color="inherit">
+            <User style={{marginRight:'30px', color: '#00c158'}} />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+       
+        <Drawer 
+          
+         
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose), 
+          }}
+          open={open} 
+         
+        >
+          <div className={classes.toolbarIcon}  style={{backgroundColor: 'white'}}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <h4 style={{textAlign: 'left', marginLeft: '20px', marginTop: '70px', marginBottom: '50px', color: 'white'}}>Hi,<br/>
+              Merchant Name
+            </h4>
+          {mainListItems}</List>
+          
+          <List>{secondaryListItems}</List>
+        </Drawer>
+        </Hidden>
+
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                
-              </Paper>
+        
+            <Grid item xs={12} md={5} lg={5}>
+              
+           
+                <div className="kwuo-bal inline">
+                 <div>
+                  <h6>CURRENT KWUO BALANCE</h6>
+                  <h2>1000.00</h2>
+                 </div>
+                  <img src={kwuobal} alt="kwuo-bal -icon" />
+                </div>
+                <div className="kwuo-bal-overlay"></div>
+              
             </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-               
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-               
-              </Paper>
+            <Grid item xs={12} md={2} lg={2}></Grid>
+            <Grid item xs={12} md={5} lg={5}>
+            <div className="table1">
+                <h3>Transaction Breakdown</h3>
+                <table className="kwuo-table">
+                  <tr>
+                    <td>Today</td>
+                    <td>This week</td>
+                    <td>This month</td>
+                  </tr>
+                  <tr>
+                    <td><b>2000</b></td>
+                    <td><b>2000</b></td>
+                    <td><b>2000</b></td>
+                  </tr>
+                </table>
+            </div>
             </Grid>
           </Grid>
-          <Box pt={4}>
+          {/*<Box pt={4}>
             <Copyright />
-          </Box>
+      </Box>*/}
+
+      <Grid container spacing={3}>
+        
+            <Grid item xs={12} md={5} lg={5}>
+              
+           
+                <div className="send-kwuo inline">
+                 <div>
+                  <h3>Send Kwuo</h3>
+                  <h6>Enjoy seamless transfers</h6>
+                 </div>
+                  <img src={sendkwuo} alt="send-kwuo-icon" />
+                </div>
+                <div className="send-kwuo-overlay"></div>
+              
+            </Grid>
+            <Grid item xs={12} md={2} lg={2}></Grid>
+            
+            <Grid item xs={12} md={5} lg={5}>
+            <div className="buy-kwuo inline">
+            <img src={sendkwuo} alt="send-kwuo-icon" />
+             <div>
+              <h4>Buy Kwuo units</h4>
+              <h6>Get more value for your money</h6>
+             </div>
+             
+            </div>
+            <div className="buy-kwuo-overlay"></div>
+          
+        </Grid>
+
+        <Grid item xs={12} md={12} lg={12}>
+        <div className="recent-transactions">
+        <h3>Recent Transactions</h3>
+        <table class="table recent-transactions">
+        <thead>
+          <tr>
+            <th scope="col">All</th>
+            <th scope="col">Received</th>
+            <th scope="col">Sent</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+        </Grid>
+        
+          </Grid>
         </Container>
       </main>
     </div>
